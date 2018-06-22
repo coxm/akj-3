e3c8fbd916128e89f0b3c7711b2deeee9dd4f887
coxm <coxm@users.noreply.github.com>
2018-06-22 22:24:40 +0100

    Remove redundant level text

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -3,12 +3,6 @@ import {Scene} from 'phaser';
 
 export class Level extends Phaser.Scene {
   create() {
-    const {width, height} = this.sys.game.config;
-    this.add.text(0.5 * width, 0.3 * height, 'Level 0', {
-      fontSize: '16px',
-      fill: '#abcde0',
-    }).setOrigin(0.5, 0.5);
-
     this.tilemap = this.make.tilemap({
       key: `tilemap:${this.sys.config.key}`,
       tileWidth: 16,
