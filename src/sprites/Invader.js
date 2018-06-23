4381c28b7ae725dc22ff04907b311912f0993952
coxm <coxm@users.noreply.github.com>
2018-06-23 19:11:44 +0100

    Set up invader spawn points

--- src/sprites/Invader.js
+++ src/sprites/Invader.js
@@ -6,5 +6,16 @@ import {tilesetName, invaderFrame} from '../settings';
 export class Invader extends GameObjects.Sprite {
   constructor(level, x, y) {
     super(level, x, y, tilesetName, invaderFrame);
+    this.speed = 0.5;
+  }
+
+  update(time, delta) {
+    const signX = Math.sign(this.target.x - this.x);
+    const signY = Math.sign(this.target.y - this.y);
+    if (signX !== 0 || signY !== 0) {
+      this.body.setVelocity(
+        Math.floor(delta * this.speed * signX),
+        Math.floor(delta * this.speed * signY));
+    }
   }
 }
