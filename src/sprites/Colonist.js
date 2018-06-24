6fc411d18541bd111853607d127e28fc2c934bdc
coxm <coxm@users.noreply.github.com>
2018-06-24 16:58:18 +0100

    Fix colonist frame error

--- src/sprites/Colonist.js
+++ src/sprites/Colonist.js
@@ -6,6 +6,6 @@ import {AnyAttacker} from './AnyAttacker';
 
 export class Colonist extends AnyAttacker {
   constructor(level, x, y, properties) {
-    super(level, x, colonistFrame, colonistProperties);
+    super(level, x, y, colonistProperties);
   }
 }
